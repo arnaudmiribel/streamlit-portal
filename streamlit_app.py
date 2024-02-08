62e6bf5d179d62d001adb618006a3fe68c751d08
Arnaud <arnaudmiribel@gmail.com>
2024-02-08 10:21:18 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,134 +1,67 @@
 import os
-import subprocess
+
 import time
 
 import streamlit as st
-from streamlit.components.v1 import html, iframe
-from streamlit_kickoff_cli.list.command import get_list
+from utils.ui import open_vs_code, show_app_in_browser
+from utils.metadata import get_apps_metadata
 
 title = "Streamlit Local Portal"
 st.set_page_config(page_icon="🖲", page_title=title)
 
-
-browser_mockup_css = """
-<style>
-.browser-mockup {
-  border-top: 2em solid rgba(230, 230, 230, 0.7);
-  box-shadow: 0 0.1em 1em 0 rgba(0, 0, 0, 0.4);
-  position: relative;
-  border-radius: 3px 3px 0 0
-}
-
-.browser-mockup:before {
-  display: block;
-  position: absolute;
-  content: '';
-  top: -1.25em;
-  left: 1em;
-  width: 0.5em;
-  height: 0.5em;
-  border-radius: 50%;
-  background-color: #f44;
-  box-shadow: 0 0 0 2px #f44, 1.5em 0 0 2px #9b3, 3em 0 0 2px #fb5;
-}
-
-.browser-mockup.with-tab:after {
-  display: block;
-  position: absolute;
-  content: '';
-  top: -2em;
-  left: 5.5em;
-  width: 20%;
-  height: 0em;
-  border-bottom: 2em solid white;
-  border-left: 0.8em solid transparent;
-  border-right: 0.8em solid transparent;
-}
-
-.browser-mockup.with-url:after {
-  display: block;
-  position: absolute;
-  content: '';
-  top: -1.6em;
-  left: 5.5em;
-  width: calc(100% - 6em);
-  height: 1.2em;
-  border-radius: 2px;
-  background-color: white;
-}
-
-.browser-mockup > * {
-  display: block;
-}
-
-</style>
-"""
-
-
-def get_app_directory(pid: str) -> str:
-
-    lsof = subprocess.check_output(
-        f"lsof -p {pid} | grep cwd",
-        shell=True,
-        stderr=subprocess.STDOUT,
-    )
-
-    return lsof.split()[-1].decode("utf-8")
-
-
-def get_apps_metadata():
-    apps_list = get_list()
-    paths = list()
-    for _, row in apps_list.iterrows():
-        pid = row.T["App ID"]
-        paths.append(get_app_directory(pid))
-    apps_list["App directory"] = paths
-    return apps_list.sort_values(by="App URL", ascending=True)
-
-
 st.title("🖲 " + title)
 
-st.write("### 🟢 Running apps")
-apps_list = get_apps_metadata().drop_duplicates(subset="App URL")
+st.write("#### 🟢 Running apps")
+apps_df = get_apps_metadata()
 
-num_apps = len(apps_list)
-st.markdown(
-    f"""Running **{num_apps}** Streamlit app{'s' if num_apps > 1 else ''} locally."""
+num_apps = len(apps_df)
+st.caption(
+    f"""Found **{num_apps}** Streamlit app{'s' if num_apps > 1 else ''} running locally."""
 )
 
-for _, row in apps_list.iterrows():
-    url = row.T["App URL"]
-    port = int(url.split(":")[-1])
-    pid = row.T["App ID"]
-    directory = row.T["App directory"]
-    if port == 8500:
-        expander_title = f"{directory.split('/')[-1]} **[this app]** - {url} - {pid}"
+for _, row in apps_df.iterrows():
+    row_dict = row.to_dict()
+    url = row_dict.get("App URL").strip()
+    port = int(row_dict.get("PORT").strip())
+    process_id = row_dict.get("App ID")
+    directory_path = row_dict.get("App directory")
+    directory_name = directory_path.split("/")[-1]
+
+    if directory_name == "streamlit-portal" and port == 8500:
+        expander_title = f"{directory_name} **[this app]** - {url} - {process_id}"
     else:
-        expander_title = f"{directory.split('/')[-1]} - {url} - {pid}"
+        expander_title = f"{directory_name} - {url} - {process_id}"
 
     with st.expander(expander_title, expanded=False):
+
         col1, col2, col3 = st.columns(3)
-        col1.markdown(f"[Open in browser]({url})")
-        open_code = col2.button("Open code", key=f"code_{pid}")
-        kill = col3.button("Kill app", key=f"kill_{pid}")
+        col1.link_button(
+            "Open in browser 🌐",
+            url,
+            use_container_width=True,
+        )
+        code_button = col2.button(
+            "Open code 👨‍💻",
+            key=f"code_{process_id}",
+            use_container_width=True,
+        )
+        kill_button = col3.button(
+            "Kill 🔫",
+            key=f"kill_{process_id}",
+            use_container_width=True,
+        )
+
         kill_status = col3.empty()
-        st.caption("Preview app:")
-        left, middle, right = st.columns((1, 4, 1))
-        with middle:
-            html(
-                browser_mockup_css
-                + '<div class="browser-mockup"><iframe src="{}" style="width:100%; border:none; height:300px;"> </iframe></div>'.format(
-                    url
-                ),
-                height=300,
-            )
-        # iframe(url, height=300)
-        if open_code:
-            os.system(f"code {directory} --new-window")
-        if kill:
+
+        show_app_in_browser(url)
+
+        if code_button:
+            open_vs_code(directory_path)
+
+        if kill_button:
             kill_status.text("Killing app...")
-            time.sleep(2)
-            os.system(f"kill -9 {pid}")
+            time.sleep(1)
+            os.system(f"kill -9 {process_id}")
             kill_status.text("Killed app! Restarting...")
-            time.sleep(2)
-            st.experimental_rerun()
+            time.sleep(1)
+            st.rerun()
